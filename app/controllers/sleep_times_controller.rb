a289f0f0b90b810ded5d1462b2146cf8721e7e41
Matt Campbell <matthewricecampbell@gmail.com>
2012-11-15 09:56:56 -0600

    Sleep Times must be linked to current_user

--- app/controllers/sleep_times_controller.rb
+++ app/controllers/sleep_times_controller.rb
@@ -1,4 +1,5 @@
 class SleepTimesController < ApplicationController
+
   def new
   	@sleep_time = SleepTime.new 
   end
@@ -9,11 +10,16 @@ class SleepTimesController < ApplicationController
   	redirect_to :root
   end
 
-def update
-
+  def update
+    @sleep_time = SleepTime.last
+    if @sleep_time.end_time = Time.new
+      @sleep_time.save!
+      redirect_to :root
+    end
   end
   
   def index
+    @completed_sleep_time = SleepTime.last
   	@sleep_time = SleepTime.last
   	if @sleep_time.nil? || @sleep_time.end_time != nil
   		@sleep_time = SleepTime.new
--- app/models/sleep_time.rb
+++ app/models/sleep_time.rb
@@ -2,16 +2,12 @@ class SleepTime < ActiveRecord::Base
   attr_accessible :start_time, :end_time
   belongs_to :user
 
-
   def sleep_duration
   	if self.end_time == nil || self.start_time == nil
   		return nil
   	end
   	sleep_seconds = self.end_time - self.start_time
   	sleep_seconds.round(0)
-  	# sleep_hours = sleep_seconds/3600
-  	# sleep_hours.round(0) 
   end
-
 end
-#if both forms are submitted do sleepduration
+
